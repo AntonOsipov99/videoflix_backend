8de5d075c91b8301952d3f471153167e25819cd7
AntonOsipov99 <antonosipov@gmx.de>
2025-02-09 16:57:01 +0100

    added django-rq to signals.py

--- videoflix_app/api/signals.py
+++ videoflix_app/api/signals.py
@@ -3,13 +3,15 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 import os
 from .tasks import convert_480p
+import django_rq
 
 @receiver(post_save, sender=Movie)
 def movie_post_save(sender, instance, created, **kwargs):
     print('Video wurde gespeichert')
     if created:
         print('New video created')
-        convert_480p(instance.video_file.path)
+        queue = django_rq.get_queue('default', autocommit=True)
+        queue.enqueue(convert_480p, instance.video_file.path)
         
 @receiver(post_delete, sender=Movie)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
