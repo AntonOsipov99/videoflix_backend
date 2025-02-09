317339762026d163cd0268e789b89faab1aa037b
AntonOsipov99 <antonosipov@gmx.de>
2025-02-09 16:24:01 +0100

    changed convert_480p function

--- videoflix_app/api/tasks.py
+++ videoflix_app/api/tasks.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def convert_480p(source):
-    base_name = source.replace('.mp4', '')
-    new_file_name = base_name + '_480.mp4'
+    # base_name = source.replace('.mp4', '')
+    new_file_name = source + '_480.mp4'
     cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)
     run = subprocess.run(cmd, capture_output=True)
