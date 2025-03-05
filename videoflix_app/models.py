456354da709a942d85960feaaaa4d7529b89d627
AntonOsipov99 <antonosipov@gmx.de>
2025-03-05 11:14:09 +0100

    changed movie model

--- videoflix_app/models.py
+++ videoflix_app/models.py
@@ -7,7 +7,7 @@ from django.core.cache import cache
 class Movie(models.Model):
     created_at = models.DateField(default=date.today, editable=False)
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=600)
     video_file = models.FileField(upload_to='videos', blank=True, null=True)
     genre = models.CharField(max_length=200, blank=True, null=True)
     image = models.FileField(upload_to='images', blank=True, null=True)
